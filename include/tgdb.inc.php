17abd222b842ed493a22c922d350aec747b4167b
root <mario@wesolek.org>
2024-03-07 09:53:33 +0100

    Add change TG from outside

---
+++ include/tgdb.inc.php
@@ -0,0 +1,22 @@
+<?php
+$tgdb_array = [ 
+'1' => 'Niederlausitz',
+'7' => 'LausitzLink Gesamt',
+'24' => 'NordLink24',
+'262' => 'Deutschland',
+'777' => 'Laberrunde',
+'1262' => 'Rundspruch',
+'2620' => 'Mecklenburg-Vorpommern',
+'2621' => 'Berlin/Brandenburg',
+'2622' => 'Hamburg / Schleswig-Holstein',
+'2623' => 'Niedersachsen / Bremen',
+'2624' => 'Nordrhein-Westfalen',
+'2625' => 'Rheinland-Pfalz / Saarland',
+'2626' => 'Hessen',
+'2627' => 'Baden-Württemberg',
+'2628' => 'Bayern',
+'2629' => 'Thüringen / Sachsen',
+'26298' => 'Thüringen',
+'262907' => 'Oberlausitz'
+];
+?>
